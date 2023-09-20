7a80bab467dc60b55b3f2680ae67f9143da9726e
Andrés <ia.andresalexander@ufg.edu.sv>
2023-09-19 19:07:20 -0600

    Api

---
+++ MiApi/Context/MyDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using MiApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiApi.Context;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Blog> Blogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=my_blogs;User Id=sa;Password=P3luch32#12;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Blog>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__blog__3213E83FB399F40B");
+
+            entity.ToTable("blog");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Author)
+                .HasMaxLength(55)
+                .IsUnicode(false)
+                .HasColumnName("author");
+            entity.Property(e => e.Likes)
+                .HasDefaultValueSql("((0))")
+                .HasColumnName("likes");
+            entity.Property(e => e.Title)
+                .HasMaxLength(55)
+                .IsUnicode(false)
+                .HasColumnName("title");
+            entity.Property(e => e.Url)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("url");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MiApi/Models/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiApi.Models;
+
+public partial class Blog
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Author { get; set; }
+
+    public string? Url { get; set; }
+
+    public int? Likes { get; set; }
+}
